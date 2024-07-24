416cec6202c8e27a4b1d2c53a80ce09c1cc52feb
joshua <ajosavboy@gmail.com>
2024-07-24 13:18:38 +0100

    Added Payment processor exception

---
+++ src/Exceptions/PaymentProcessorException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ajosav\Blinqpay\Exceptions;
+
+use PHPUnit\Event\Code\Throwable;
+
+class PaymentProcessorException extends \Exception
+{
+    /**
+     * @param string $message
+     * @param ?int $status_code
+     * @param ?Throwable $previous
+     */
+    public function __construct(string $message, ?int $status_code = 500, ?Throwable $previous = null)
+    {
+        parent::__construct(message: $message, code: $status_code, previous: $previous);
+    }
+}
